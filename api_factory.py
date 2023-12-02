fc10c069ecb20f27e777a174cb440347a4b912c3
Maiara Lewis Cipriano <mlewisc@umich.edu>
2023-12-02 10:31:15 -0500

    comments api_factory.py

--- api_factory.py
+++ api_factory.py
@@ -2,13 +2,13 @@ import apicredentials
 import requests
 
 
-#get movie data from OMDb API for all the movies in a list 
+# Get movie data from OMDb API for all the movies in a list 
 def get_omdbapi_movie_data(movie_list):
     
     api_key = apicredentials.omdb_apikey
     base_url = "http://www.omdbapi.com/"
 
-    #put the parameters for the resquest in a dictionary
+    # Put the parameters for the resquest in a dictionary
     params_dict = {"apikey": api_key, "t": ""}
 
     all_movies_useful_info = []
@@ -16,53 +16,53 @@ def get_omdbapi_movie_data(movie_list):
     
     for movie in movie_list: 
 
-        #define the movie title in the dictionary to the given movie in the list
+        # Define the movie title in the dictionary to the given movie in the list
         params_dict["t"] = movie
 
-        #get movie data
+        # Det movie data
         response = requests.get(base_url, params=params_dict)
         movie_data = response.json()
         
         try:
-            #gather useful information
+            # Gather useful information
             year = movie_data["Year"]
             rotten_rating = (movie_data["Ratings"][1]["Value"]).strip("%")
             metascore_rating = int(movie_data["Metascore"])
             imdb_rating = float(movie_data["imdbRating"])
             actors_list = (movie_data["Actors"]).split(", ")
 
-            #create a tuple with the movie's useful information
+            # Create a tuple with the movie's useful information
             movie_details = (movie, int(year), int(rotten_rating), metascore_rating, imdb_rating, actors_list)
-            
+
             all_movies_useful_info.append(movie_details)
 
         except:
-            #insert movie name into the list of movie with no information found on the API
+            # Insert movie name into the list of movie with no information found on the API
             movies_with_no_info_found.append(movie)
 
             
     return all_movies_useful_info
 
 
+# Get actor data from OMDb API for all the actors in a list 
 def get_celebrityapi_actors_data(actors_list):
 
     apikey = apicredentials.celebrity_apikey
-    #base_url = 'https://api.api-ninjas.com/v1/celebrity?name'
 
     actors_useful_information = []
     actors_not_found = []
 
-    #Get the actors data from the API
+    # Get the actors data from the API
     for actor in actors_list:
         url = 'https://api.api-ninjas.com/v1/celebrity?name={}'.format(actor)
         response = requests.get(url, headers={'X-Api-Key': apikey})
         actor_data = response.json()
         
 
-        #Make sure the actor information was found
+        # Make sure the actor information was found
         try:
             actor_info = actor_data[0]
-            #extract the useful data
+            # Extract the useful data
             net_worth = actor_info["net_worth"]
             gender = actor_info["gender"]
             birthday = actor_info["birthday"]
@@ -70,13 +70,13 @@ def get_celebrityapi_actors_data(actors_list):
             is_alive = actor_info["is_alive"]
 
 
-            #create a tuple with all actor's useful details
+            # Create a tuple with all actor's useful details
             actor_details = (actor, age, gender, birthday, net_worth, is_alive)
 
-            #Add all actors tuples to a list
+            # Add all actors tuples to a list
             actors_useful_information.append(actor_details)
 
-        #Keep track of the actors that were not found in the API
+        # Keep track of the actors that were not found in the API
         except:
             actors_not_found.append(actor)   
 
