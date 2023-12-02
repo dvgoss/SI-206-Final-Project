7b572fc291dafd99cf2d0b5415c34642c1b53725
Maiara Lewis Cipriano <mlewisc@umich.edu>
2023-12-02 00:05:06 -0500

    implement add_celebrityapi_data_to_databseclear

--- database-utilities.py
+++ database-utilities.py
@@ -15,7 +15,7 @@ def setup_all_tables(cur, conn):
     cur.execute("CREATE TABLE IF NOT EXISTS Movies (movie_id INTEGER PRIMARY KEY, title TEXT UNIQUE, year INTEGER, rotten_tomatoes FLOAT, metascore FLOAT, imdb FLOAT)")
     
     #create Actors table
-    cur.execute("CREATE TABLE IF NOT EXISTS Actors (actor_id INTEGER PRIMARY KEY, name TEXT UNIQUE, age INTEGER, gender TEXT, birthday TEXT, net_worth NUMERIC, is_alive)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Actors (actor_id INTEGER PRIMARY KEY, name TEXT UNIQUE, age INTEGER, gender TEXT, birthday TEXT, net_worth NUMERIC, is_alive INTEGER)")
     
     #create Movies_Actors table
     cur.execute("CREATE TABLE IF NOT EXISTS Movies_and_Actors (movie_actor_combination_id INTEGER PRIMARY KEY, movie_id INTEGER, actor_id INTEGER)")
@@ -80,8 +80,48 @@ def get_last_actors_id(cur):
     return last_actor_id
 
 
+def add_celebrityapi_data_to_database(index, cur, conn):
+
+    # Gather all actors that were added to the database in the current run
+    cur.execute("SELECT name FROM Actors WHERE actor_id > (?)", (index,))
+    all_actors_rows = cur.fetchall()
+
+    # Create a list of actors to request data from the celebrity API
+    list_of_actors = []
+    for row in all_actors_rows:
+        actor_name = row[0]
+
+        list_of_actors.append(actor_name)
+
+    
+    # Get the data from the celebrity API
+    actors_info, no_info_actors = (api_factory.get_celebrityapi_actors_data(list_of_actors))
+    
+    for actor in actors_info:
+        name, age, gender, birthday, net_worth, is_alive = actor
+        
+        # Convert boolean to SQLite appropriate
+        if is_alive == True:
+            is_alive = 1
+        else:
+            is_alive = 0
+
+
+        # Update columns in the Actors table for the given actor
+        #cur.execute("UPDATE Actors SET age=?, gender=?, birthday=?, net_worth=?, is_alive=? WHERE name=?",(age, gender, birthday, net_worth, is_alive, name))
+        #conn.commit()
+
+        for actor in no_info_actors:
+
+            # Delete rows of Actors where the API didn't have information on
+            cur.execute("DELETE FROM Actors WHERE name = (?)", (actor,))
+            conn.commit()
+
+
+
 cur, conn = setup_database_structure("Movie & Actors Database")
 setup_all_tables(cur, conn)
-movie_data = [("movie 1", 2012, 70, 69, 4.5, ["Actor 1", "Actor 2", "Actor 3"]), ("movie 2", 2022, 89, 75, 8.9, ["Actor 1", "Actor 4", "Actor 5"]), ("movie 3", 2022, 89, 75, 8.9, ["Actor 8", "Actor 9", "Actor 10"])]
+index = get_last_actors_id(cur)
+movie_data = [("movie 1", 2012, 70, 69, 4.5, ["Lindsay Lohan", "Leonardo Dicaprio", "Amber Heard"]), ("movie 2", 2022, 89, 75, 8.9, ["Lindsay Lohan", "Rachael McAdams", "Meryl Streep"]), ("movie 3", 2022, 89, 75, 8.9, ["George Clooney", "Angelina Jolie", "Brad Pitt"])]
 add_omdbapi_data_to_database(movie_data, cur, conn)
-get_last_actors_id(cur)
+add_celebrityapi_data_to_database(index, cur, conn)
