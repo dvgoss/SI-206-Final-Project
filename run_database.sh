657705bab18606a7f8a3d804cfbe78f3403db3b5
Maiara Lewis Cipriano <mlewisc@umich.edu>
2023-12-04 18:11:24 -0500

    add comment to run_database.sh

--- run_database.sh
+++ run_database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Run the database-utilities file enough times (85) to fill the database with data for all the movies in the passed list. 
 for i in $(seq 1 85); do
   python3 database-utilities.py
   echo "Finished running database-utilities $i times"
